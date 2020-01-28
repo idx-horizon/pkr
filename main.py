43a7eba0091f5bc402f9b4a884f095b1bf76b2a6
idx <idx-horizon@users.noreply.github.com>
2020-01-28 08:28:33 +0000

    fix

--- main.py
+++ main.py
@@ -67,14 +67,14 @@ def r_events(country=None, filter_str=None):
     if request.method.upper() == 'POST':
       this_filter  = str(request.form['filter_str']).lower()
       this_method  = str(request.form['filter_method'])
-      this_country = str(request.form['country'])
+      this_country = str(request.form['country_code'])
       
     print('** Request: [{}] [{}]'.format(request.url, request.method))
     print('** method [{}] country [{}] string [{}]'.format(this_method, this_country, this_filter))
     
     data=NR.getevents_by_filter(this_filter, this_country, this_method)
     return render_template('events.html',
-                title=get_app_title() + '[' + this_filter + ']', 
+                title=get_app_title() + '[' + this_country + ' | ' + this_filter + ']', 
                 filter=this_filter, 
                 filter_method=this_method,
                 file_modified_date=NR.get_last_update(),
