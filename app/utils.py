94b8dedc619f8d8874d1f4a409413f5db8220454
idx <idx-horizon@users.noreply.github.com>
2020-02-11 13:08:46 +0000

    years running challenge

--- app/utils.py
+++ app/utils.py
@@ -102,7 +102,10 @@ class Runner():
 		challenges['Wilson-index'] = wix
 		
 		challenges['Parkrun birthday'] = self.runs[-1]['Run Date']
-		challenges['Years running'] = 'tbc'						
+		
+		yr = sorted([x.replace('_YR_',,) for x in self.stats if x.startswith('_YR_')])
+		challenges['Years running'] = '{} ({} to {})'.format(len(yr), yr[0], yr[-1])
+			
 		challenges['Tourist Quotient'] = '{:%}'.format(
 												len([x for x in self.stats if x.startswith('_EVENT_')]) / self.run_count
 											)
