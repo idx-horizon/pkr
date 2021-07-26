ac5f00ec4fe1a0d2e15c8652a8e9955103cf0c44
idx <idx-horizon@users.noreply.github.com>
2021-07-26 11:26:25 +0100

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 #from flask_migrate import Migrate
 #from flask_bootstrap import Bootstrap
 
-from app import models
+#from app import models
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config.from_object(Config)
