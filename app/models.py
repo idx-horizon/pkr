8fd54eb6dedc5dbf486cc0794a96744c712eec57
idx <idx-horizon@users.noreply.github.com>
2021-07-26 11:15:22 +0100

    fix

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from app import login  #, flaskdb
+#from app import login, flaskdb
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
