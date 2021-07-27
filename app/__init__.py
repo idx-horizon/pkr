2e5f1ea6b37e19b4485995432180a8a608c7f738
idx <idx-horizon@users.noreply.github.com>
2021-07-27 07:51:04 +0100

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager
 from app.config import Config
 
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+#from flask_migrate import Migrate
 #from flask_bootstrap import Bootstrap
 
 
