eb2491c52224c51fcba4191df333d912d7086c86
idx <idx-horizon@users.noreply.github.com>
2021-07-29 20:31:25 +0100

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager
 from app.config import Config
 
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+#from flask_migrate import Migrate
 #from flask_bootstrap import Bootstrap
 
 
@@ -12,7 +12,7 @@ app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+#migrate = Migrate(app, db)
 
 #bootstrap = Bootstrap(app)
 
