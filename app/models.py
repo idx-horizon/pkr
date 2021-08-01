c4b16d272ec00535c1cd91de8778c69b4b7db7ef
idx <idx-horizon@users.noreply.github.com>
2021-08-01 21:03:17 +0100

    Change to user UserMixin in User model

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import login, db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+class User(UserMixin):
     id            = db.Column(db.Integer, primary_key=True)
     username      = db.Column(db.String(64), index=True, unique=True)
     rid           = db.Column(db.String(10), index=True, unique=True)
