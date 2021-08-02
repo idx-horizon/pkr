b3e6bcb94a70102a40b642ed6d96731d3bbc6e0e
idx <idx-horizon@users.noreply.github.com>
2021-08-02 17:38:33 +0100

    fix

--- app/summaries.py
+++ app/summaries.py
@@ -5,12 +5,21 @@ def year_summary(runs):
     c = Counter()
     for r in runs: 
         c[r['Run Date'][-4:]] += 1
-    return sorted([{'year': k,'count': v} for k,v in c.items()],key=attrgetter('year'))        
+        
+    data = [{'year': k,'count': v} for k,v in c.items()]
+    
+    return sorted(data,key=attrgetter('year'))        
+
     
 def event_summary(runs):
     c = Counter()
     for r in runs: 
         c[r['Event']] += 1
+
+    data = [{'year': k,'count': v} for k,v in c.items()]
+
+    return sorted(data,reverse=True, key=attrgetter('count'))        
         
 #    return [{'event': k,'count': v} for k,v in c.items()]        
-    return sorted([{'year': k,'count': v} for k,v in c.items()],reverse=True, key=attrgetter('count'))        
+
+#    return sorted([{'year': k,'count': v} for k,v in c.items()],reverse=True, key=attrgetter('count'))        
