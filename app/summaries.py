a8697ec4694c9f2b6b94fe5fc9b2b9fdf60a24a6
idx <idx-horizon@users.noreply.github.com>
2021-08-02 18:04:12 +0100

    cosmetic

--- app/summaries.py
+++ app/summaries.py
@@ -1,5 +1,4 @@
 from collections import Counter
-from operator import attrgetter
 
 def year_summary(runs):
     c = Counter()
@@ -9,19 +8,12 @@ def year_summary(runs):
     data = [{'year': k,'count': v} for k,v in c.items()]
 
     return sorted(data,key=lambda x: x['year'], reverse=True)     
-#    return sorted(data,key=attrgetter('year'))        
-
     
 def event_summary(runs):
     c = Counter()
     for r in runs: 
         c[r['Event']] += 1
 
-    data = [{'year': k,'count': v} for k,v in c.items()]
+    data = [{'event': k,'count': v} for k,v in c.items()]
 
     return sorted(data,key=lambda x: x['count'], reverse=True)     
-#    return sorted(data,reverse=True, key=attrgetter('count'))        
-        
-#    return [{'event': k,'count': v} for k,v in c.items()]        
-
-#    return sorted([{'year': k,'count': v} for k,v in c.items()],reverse=True, key=attrgetter('count'))        
