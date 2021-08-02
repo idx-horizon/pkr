cdfab5671c7b0572a7c530338c649623a4168456
idx <idx-horizon@users.noreply.github.com>
2021-08-02 17:58:13 +0100

    fix sort

--- app/summaries.py
+++ app/summaries.py
@@ -7,8 +7,9 @@ def year_summary(runs):
         c[r['Run Date'][-4:]] += 1
         
     data = [{'year': k,'count': v} for k,v in c.items()]
-    
-    return sorted(data,key=attrgetter('year'))        
+
+    return sorted(x,key=lambda x: x['year'], reverse=True)     
+#    return sorted(data,key=attrgetter('year'))        
 
     
 def event_summary(runs):
@@ -18,7 +19,8 @@ def event_summary(runs):
 
     data = [{'year': k,'count': v} for k,v in c.items()]
 
-    return sorted(data,reverse=True, key=attrgetter('count'))        
+    return sorted(x,key=lambda x: x['count'], reverse=True)     
+#    return sorted(data,reverse=True, key=attrgetter('count'))        
         
 #    return [{'event': k,'count': v} for k,v in c.items()]        
 
