90a275fa5c8836a7fc4dd958269da87622fe5cf3
idx <idx-horizon@users.noreply.github.com>
2021-08-02 13:38:14 +0100

    default to logged in  user home_run on events form

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def r_events(country=None, filter_str=None, centre_on_code=None):
     this_country = country or 'uk'	
     this_filter = filter_str or ''
     this_method = 'startswith'
-    this_centre_on = centre_on_code or 'bromley'
+    this_centre_on = centre_on_code or current_user.home_run or 'bushy'
 
     if request.method.upper() == 'POST':
       this_filter  = str(request.form['filter_str']).lower()
