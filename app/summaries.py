442896e6a196b01e1ac808bc64549da67e2d5a86
idx <idx-horizon@users.noreply.github.com>
2021-08-09 22:27:36 +0100

    fix

--- app/summaries.py
+++ app/summaries.py
@@ -8,7 +8,7 @@ def get_avg(lst):
 
 def get_ystat_times(runs, k):
 
-    l = [x['Time'] if len(x)==5 else x + '00' for x in runs if x['Run Date'][-4:] == k]
+    l = [x['Time'] if len(x['Time'])==5 else x['Time'] + '00:' for x in runs if x['Run Date'][-4:] == k]
     return ( get_avg(l), min(l), max(l) )
     
 def get_estat_times(runs, k):
