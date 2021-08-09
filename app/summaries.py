5669e96dc50a12b3dcf2b010f258644430cd2f03
idx <idx-horizon@users.noreply.github.com>
2021-08-09 08:50:28 +0100

    Fix

--- app/summaries.py
+++ app/summaries.py
@@ -1,11 +1,15 @@
 from collections import Counter
 
+# t1 = map(lambda  x:  (int(x['Time'][-5:-3])*60) + int(x['Time'][-2:]), runs)
+
 def year_summary(runs):
     c = Counter()
     for r in runs: 
         c[r['Run Date'][-4:]] += 1
         
-    data = [{'year': k,'count': v} for k,v in c.items()]
+    data = [{'year': k,'count': v,
+             'fastest': '?', 
+             'slowest': '?'} for k,v in c.items()]
 
     return sorted(data,key=lambda x: x['year'], reverse=True)     
     
