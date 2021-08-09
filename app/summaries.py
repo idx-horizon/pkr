bc8f7260ba4a47192ea1d5b1b4caf803ed107f2c
idx <idx-horizon@users.noreply.github.com>
2021-08-09 10:08:06 +0100

    fix

--- app/summaries.py
+++ app/summaries.py
@@ -2,11 +2,15 @@ from collections import Counter
 
 # t1 = map(lambda  x:  (int(x['Time'][-5:-3])*60) + int(x['Time'][-2:]), runs)
 
-def get_stat_times(runs, k):
+def get_ystat_times(runs, k):
 
     l = [x['Time'] for x in runs if x['Run Date'][-4:] == k]
     return ( '-', min(l), max(l) )
     
+def get_estat_times(runs, k):
+
+    l = [x['Time'] for x in runs if x['Event'] == k]
+    return ( '-', min(l), max(l) )
     
 def year_summary(runs):
     c = Counter()
@@ -16,7 +20,7 @@ def year_summary(runs):
         
     data = [{'year': k,
              'count': v,
-             'times': get_stat_times(runs, k), 
+             'times': get_ystat_times(runs, k), 
              } for k,v in c.items()]
 
     return sorted(data,key=lambda x: x['year'], reverse=True)     
@@ -26,9 +30,9 @@ def event_summary(runs):
     for r in runs: 
         c[r['Event']] += 1
 
-    data = [{'event': k,'count': v,
-             'fastest': '?', 
-             'slowest': '?', 
-             'average': '?'} for k,v in c.items()]
+    data = [{'event': k,
+             'count': v,
+             'times': get_estat_times(runs, k), 
+             } for k,v in c.items()]
 
     return sorted(data,key=lambda x: x['count'], reverse=True)     
