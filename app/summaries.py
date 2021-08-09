4f3e0f795354e5456f55f3e1cbe508fe745b72ac
idx <idx-horizon@users.noreply.github.com>
2021-08-09 10:03:05 +0100

    summary times

--- app/summaries.py
+++ app/summaries.py
@@ -2,10 +2,10 @@ from collections import Counter
 
 # t1 = map(lambda  x:  (int(x['Time'][-5:-3])*60) + int(x['Time'][-2:]), runs)
 
-def get_stat_time(runs, k):
+def get_stat_times(runs, k):
 
     l = [x['Time'] for x in runs if x['Run Date'][-4:] == k]
-    return ( min(l), max(l) )
+    return ( '-', min(l), max(l) )
     
     
 def year_summary(runs):
@@ -14,10 +14,10 @@ def year_summary(runs):
         c[r['Run Date'][-4:]] += 1
     
         
-    data = [{'year': k,'count': v,
-             'fastest': get_stat_time(runs, k), 
-             'slowest': '?', 
-             'average': '?'} for k,v in c.items()]
+    data = [{'year': k,
+             'count': v,
+             'times': get_stat_times(runs, k), 
+             } for k,v in c.items()]
 
     return sorted(data,key=lambda x: x['year'], reverse=True)     
     
