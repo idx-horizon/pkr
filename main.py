a92ce536ca02cd6575243a4122454212540e0a73
idx <idx-horizon@users.noreply.github.com>
2021-08-29 18:52:19 +0100

    diag

--- main.py
+++ main.py
@@ -77,14 +77,15 @@ def apilog():
 def r_graph():
     try:
        graph = pygal.Line()
-       graph.title = '% Change Coolness of programming languages over time.'
-       graph.x_labels = ['2011','2012','2013','2014','2015','2016']
-       graph.add('Python',  [15, 31, 89, 200, 356, 900])
-       graph.add('Java',    [15, 45, 76, 80,  91,  95])
-       graph.add('C++',     [5,  51, 54, 102, 150, 201])
-       graph.add('All others combined!',  [5, 15, 21, 55, 92, 105])
+       graph.title = 'Languages over time.'
+       graph.x_labels = ['2012','2013','2014','2015','2016']
+       graph.add('Python',  [15, 31, 89, 200, 356])
+       graph.add('Java',    [15, 45, 76, 80,  91])
+       graph.add('C++',     [5,  51, 54, 102, 150])
        graph_data = graph.render_data_uri()
-       return render_template("graph.html", graph_data = graph_data)
+       return render_template("graph.html", 
+                                graph_title = graph.title
+                                graph_data = graph_data)
     except Exception as e:
        return(str(e))
       
