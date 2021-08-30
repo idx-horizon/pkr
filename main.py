ab4c74e8f7cf9f473994f16760d9e6e4cce575bf
idx <idx-horizon@users.noreply.github.com>
2021-08-30 22:34:41 +0100

    diag

--- main.py
+++ main.py
@@ -136,7 +136,8 @@ def r_events(country=None, filter_str=None, centre_on_code=None):
       this_country = str(request.form['country_code'])
       this_centre_on = str(request.form['centre_on_code'])
 
-    print('** args', request.args.get('has_run'))
+    print('** has_run args', request.args.get('has_run','not set'))
+    print('** all args', request.args)
     
     data=NR.getevents_by_filter(this_filter, country_dict[this_country]['id'], this_method, this_centre_on)
     data = sorted(data, key=attrgetter('distance'))
