78d2354fee9fb3cc216380b5d0d0ed72cace13c6
idx <idx-horizon@users.noreply.github.com>
2021-08-31 23:12:28 +0100

    fix

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def make_shell_context():
 @app.context_processor
 def inject_context():
     return dict(selected_runner=session['SELECTEDRUNNER'],
-                friends=session['FRIENDS'],
+                friends=None, #session['FRIENDS'],
                 title = os.environ['APP_TITLE'])
     
 @app.template_filter()
@@ -254,7 +254,7 @@ def login():
             
         LoginLog.add(form.username.data.lower(), request.headers['X-Real-IP'])
         session['SELECTEDRUNNER'] = {'username': user.username, 'rid': user.rid}
-        session['FRIENDS'] = Friend.get(user.username)
+        #session['FRIENDS'] = Friend.get(user.username)
         return redirect(next_page)
 
     return render_template('login.html', title='Login', form=form)
