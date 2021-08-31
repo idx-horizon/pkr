b7170cfd1c9b6b35bbaa49aaa72fcc97ef51446d
idx <idx-horizon@users.noreply.github.com>
2021-08-31 15:34:22 +0100

    helper: addfriend

--- app/helpers.py
+++ app/helpers.py
@@ -14,3 +14,10 @@ def seticon():
           x = User.query.filter_by(username=u[0]).first()
           x.icon = u[1]
           db.session.commit()
+
+def add_f(f, t):
+     to = User.query.filter_by(username=t).first
+     friend = Friend(f_username=t,f_rid=to.rid, u_username=f)
+     db.session.add(friend)
+     db.session.commit()
+      
