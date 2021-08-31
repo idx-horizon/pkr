234c1161566c382d08091796589fee330e8a3a16
idx <idx-horizon@users.noreply.github.com>
2021-08-31 15:59:55 +0100

    helpers

--- app/helpers.py
+++ app/helpers.py
@@ -2,17 +2,20 @@ from app import db
 from app.models import User, Friend
 
 def seticon():
-     users = [('ian', '🆔'),
-          ('michael', '🏃‍♂️'),
-          ('sam',     '🏃‍♂️'),
-          ('ant',     '🏃‍♂️'),
-          ('matt',    '🏃‍♂️'),
-          ('caroline','✨')
+     users = [('ian', '🆔', 47.0),
+          ('michael', '🏃‍♂️', 50.0),
+          ('sam',     '🏃‍♂️', 50.0),
+          ('ant',     '🏃‍♂️', 50.0),
+          ('matt',    '🏃‍♂️', 50.0),
+          ('eileen',    '🏃', 50.0),
+          ('sharon',    '🏃', 50.0),
+          ('caroline','✨', 50.0)
      ]
      
      for u in users:
           x = User.query.filter_by(username=u[0]).first()
           x.icon = u[1]
+          x.agegrade_theshold = u[2]
           db.session.commit()
 
 def add_f(f, t):
@@ -20,4 +23,17 @@ def add_f(f, t):
      friend = Friend(f_username=t,f_rid=to.rid, u_username=f)
      db.session.add(friend)
      db.session.commit()
-      
+
+def all_u():
+     for u in us: 
+          print('{:<10} {:<8} {:<15} {:<5} {:<6}'.format(
+               u.username, 
+               u.rid, 
+               u.home_run, 
+               str(u.is_admin), 
+               str(u.agegrade_theshold)))
+               
+def agegrade(u,value):
+     to = User.query.filter_by(username=t).first()
+     to.agegrade_theshold = value
+     db.session.commit()
