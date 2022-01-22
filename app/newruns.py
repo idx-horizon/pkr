8f542b98d2dd7dcce4b91742cc44758b0b7ef312
idx <idx-horizon@users.noreply.github.com>
2022-01-22 18:49:16 +0000

    fix

--- app/newruns.py
+++ app/newruns.py
@@ -21,6 +21,7 @@ class Event():
         self.evshortname = event['properties']['EventShortName']
         self.evlongname = event['properties']['EventLongName']
         self.first_run = event['properties']['first_run']
+        self.sss_score = event['properties']['sss_score']
         self.latitude = event['geometry']['coordinates'][1]
         self.longitude = event['geometry']['coordinates'][0]        
         self.domain = 'https://' + [country_dict[ele]['base'] for ele in country_dict if country_dict[ele]['id']==event['properties']['countrycode']][0] +'/'
