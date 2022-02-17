cf16b96eeae425e24ab92098a6cfac5d6145d39f
idx <idx-horizon@users.noreply.github.com>
2022-02-17 13:59:36 +0000

    xltables - wip

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ def show_tables():
     data.index.name=None
     females = data.loc[data.Gender=='f']
     males = data.loc[data.Gender=='m']
-    return render_template('xlview.html',tables=[females.to_html(classes='female'), males.to_html(classes='male')],
-    titles = ['na', 'Female surfers', 'Male surfers'])
+    return render_template('xlview.html',tables=[females.to_html(classes='female'), males.to_html(classes='male'), data.to_html()],
+    titles = ['na', 'Female', 'Male', 'All'])
 
 
 
