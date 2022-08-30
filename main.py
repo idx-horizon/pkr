62b2aa4d5366cec74199b356bf247aae2cc3cfd7
idx <idx-horizon@users.noreply.github.com>
2022-08-30 09:24:04 +0100

    fix

--- main.py
+++ main.py
@@ -125,27 +125,7 @@ def r_mapview():
         lng=-122.1419,
         markers=[(37.4419, -122.1419)]
     )
-    sndmap = Map(
-        identifier="sndmap",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-             'lat': 37.4419,
-             'lng': -122.1419,
-             'infobox': "<b>Hello World</b>"
-          },
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-             'lat': 37.4300,
-             'lng': -122.1400,
-             'infobox': "<b>Hello World from other place</b>"
-          }
-        ]
-    )
-    return render_template('mapview.html', mymap=mymap, sndmap=sndmap)
-
+    return render_template('mapview.html', mymap=mymap)
 
 
 @app.route('/api/v1/meta', methods=['POST', 'GET'])
