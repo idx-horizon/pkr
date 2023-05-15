96e28ed81d12907457e74428a204dddb56f64ebb
idx <idx-horizon@users.noreply.github.com>
2023-05-15 21:37:33 +0100

    fix

--- main.py
+++ main.py
@@ -109,11 +109,11 @@ def index():
 def r_refresh():
 #    d = refresh_events('events.json')
     d = add_anniversary_and_stats_info('events.json')
-    if d.ok:
-        return {'Success': d.ok, 
-                'Version': 'v' + os.environ['PKR_VERSION']}
-    else:
-        return {'Status Code': d.status_code}
+#    if d.ok:
+    return {'Success': len(d), 
+            'Version': 'v' + os.environ['PKR_VERSION']}
+#    else:
+#        return {'Status Code': d.status_code}
 
 @app.route('/health')
 @app.route('/health/')
