c5623f8417bc5c6b3f84e25e5ff2aea70e5478b8
idx <idx-horizon@users.noreply.github.com>
2025-01-03 17:15:59 +0000

    Add OAuth to app

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
-from flask import Flask
-
+from flask import Flask, redirect, url_for, session
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+from authlib.integrations.flask_client import OAuth
+
 from app.config import Config
 
-from flask_sqlalchemy import SQLAlchemy
 try:
     from flask_migrate import Migrate
 except:
@@ -26,6 +28,20 @@ except:
     print('Unable to print GM key')
      
 GoogleMaps(app)
+
+oauth = OAuth(app)
+pool=os.environ['MY_COG_POOL']
+authority=f'https://cognito-idp.eu-west-2.amazonaws.com/{pool}',
+metadata=f'{authority}/.well-known/openid-configuration'
+
+oauth.register(
+    name='odic',
+    authority=authority,
+    client_id=client_id,
+    client_secret=client_secret,
+    server_metadata_url=metadata
+    client_kwargs={'scope': 'email openid phone'}
+)
 
 db = SQLAlchemy(app)
 try:
